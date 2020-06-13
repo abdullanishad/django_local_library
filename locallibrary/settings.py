6889687a0f4916ea292641efddb609ac9ce0873b
abdullanishad <abdullanishad00@gmail.com>
2020-06-13 17:24:10 +0530

    changed old appname to new appname

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'abdulla88uv8o9cvi85obwf$n5s=_=
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['ancient-lake-65198.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['anlibrary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
